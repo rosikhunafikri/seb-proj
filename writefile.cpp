65d61d9e6f7e69f8582ad77bfc0fcf08218f1229
Rosikhuna Fikri Assafari <rosikhuna@voltras.co.id>
2024-10-30 01:20:59 +0700

    add write files

---
+++ writefile.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  // Create and open a text file
+  ofstream MyFile("filename123.txt");
+
+  // Write to the file
+  MyFile << "Files can be tricky, but it is fun enough!";
+
+  cout << "cout between lines" << endl;
+
+  MyFile << "second liiiineee!";
+
+
+  // Close the file
+  MyFile.close();
+}
